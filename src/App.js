10f9c5f72e7ab19a9156f1013a76067a645d5a57
Emmanuel Abobila <masterxdee@gmail.com>
2023-11-06 16:04:42 +0000

    configured routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./AuthenticationPage/login";
 import Register from "./AuthenticationPage/Register";
 import HomePage from "./Pages/HomePage";
+import ProductDetails from "./Pages/HomePageContent/ProductsDetails";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/product-details" element={<ProductDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- src/AuthenticationPage/Register.js
+++ src/AuthenticationPage/Register.js
@@ -1,148 +1,156 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
+import { Link } from "react-router-dom";
+
+const initialValues = {
+  firstname: "",
+  lastname: "",
+  phone: "",
+  email: "",
+  password: "",
+  cpassword: "",
+};
 
 function Register() {
-  return (
-    <Formik
-      initialValues={{
-        firstname: "",
-        surname: "",
-        phonenumber: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      }}
-      validationSchema={Yup.object({
-        email: Yup.string()
-          .email("Invalid email address")
-          .required("Please enter email"),
-        password: Yup.string().required("Please enter password"),
-        firstname: Yup.string().required("Please enter first name"),
-        surname: Yup.string().required("Please enter last name"),
-        confirmPassword: Yup.string().required("please confirm password"),
-        phonenumber: Yup.string().required("Please enter phone number"),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      <Form>
-        <div className="flex justify-center items-center h-screen bg-[#247158] ">
-          <div className="flex flex-row bg-white w-[65%] rounded-lg p-1">
-            <div className="w-[50%]">
-              <img
-                className="w-[100%] "
-                src="./asset/register.jpg"
-                alt="Registration"
-              />
-            </div>
+  const registerSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Please enter email address"),
+    firstname: Yup.string().required("Please enter first name"),
+    lastname: Yup.string().required("Last name cannot be left blank"),
+    password: Yup.string().required("Please enter password"),
+    cpassword: Yup.string().required("passwords do not match"),
+    phone: Yup.string().required("Please enter phone number"),
+  });
 
-            <div className="flex flex-col w-[50%] p-5 justify-center">
-              <h1 className="text-4xl font-bold text-[#247158] text-center mb-5">
-                Create Account
-              </h1>
-              <div className=" flex flex-col text-left">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="text"
-                  id="firstname"
-                  name="firstname"
-                  placeholder="First Name"
-                />
-                <ErrorMessage
-                  name="firstname"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className=" flex flex-col text-left">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="text"
-                  id="surname"
-                  name="surname"
-                  placeholder="Last Name"
-                />
-                <ErrorMessage
-                  name="surname"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className=" flex flex-col text-left">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className=" flex flex-col text-left">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className=" flex flex-col text-left">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="password"
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  placeholder="Confirm Password"
-                />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className=" flex flex-col text-left ">
-                <Field
-                  className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                  type="text"
-                  id="phonenumber"
-                  name="phonenumber"
-                  placeholder=" Phone Number"
-                />
-                <ErrorMessage
-                  name="phonenumber"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="text-white rounded-md mt-3 bg-[#247158] font-bold w-full p-2"
-                >
-                  Register
-                </button>
-                <p className="text-gray-500  mt-2 ">Already have an account?</p>
-              </div>
+  const { values, handleSubmit, handleBlur, handleChange, errors, touched } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: registerSchema,
+      onSubmit: (values) => {
+        console.log(values);
+      },
+    });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex justify-center items-center h-screen bg-[#247158] ">
+        <div className="flex flex-row bg-white w-[65%] rounded-lg p-1">
+          <div className="w-[50%]">
+            <img
+              className="w-[100%] "
+              src="./asset/register.jpg"
+              alt="Registration"
+            />
+          </div>
+
+          <div className="flex flex-col w-[50%] p-5 justify-center">
+            <h1 className="text-4xl text-[#247158] text-center mb-5">
+              Create Account
+            </h1>
+            <div className=" flex flex-col text-left">
+              <input
+                type="text"
+                placeholder="enter first name"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="firstname"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstname}
+              />
+              {touched.firstname && errors.firstname ? (
+                <small className="text-red-500">{errors.firstname}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left">
+              <input
+                type="text"
+                placeholder="enter last name"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="firstname"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastname}
+              />
+              {touched.lastname && errors.lastname ? (
+                <small className="text-red-500">{errors.lastname}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left">
+              <input
+                type="email"
+                placeholder="enter email"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="firstname"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+              />
+              {touched.email && errors.email ? (
+                <small className="text-red-500">{errors.email}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left">
+              <input
+                type="text"
+                placeholder="enter password"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+              />
+              {touched.password && errors.password ? (
+                <small className="text-red-500">{errors.password}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left">
+              <input
+                type="password"
+                placeholder="confirm your password"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="cpassword"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.cpassword}
+              />
+              {touched.cpassword && errors.cpassword ? (
+                <small className="text-red-500">{errors.cpassword}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left ">
+              <input
+                type="text"
+                placeholder="enter phone number"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="phone"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.phone}
+              />
+              {touched.phone && errors.phone ? (
+                <small className="text-red-500">{errors.phone}</small>
+              ) : null}
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="text-white rounded-md mt-3 bg-[#247158] font-bold w-full p-2"
+              >
+                Register
+              </button>
+              <p className="text-gray-500  mt-2 ">
+                Already have an account?{" "}
+                <Link to={"/login"} className="text-[#2e8b36]">
+                  Login here
+                </Link>
+              </p>
             </div>
           </div>
         </div>
-      </Form>
-    </Formik>
+      </div>
+    </form>
   );
 }
 
