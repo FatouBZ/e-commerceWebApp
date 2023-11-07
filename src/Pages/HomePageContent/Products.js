e645bc11981d0da60669ff320b26d82b1f29e40b
Emmanuel Abobila <masterxdee@gmail.com>
2023-11-07 13:48:34 +0000

    hidden scrollbars added

--- src/Pages/HomePageContent/Products.js
+++ src/Pages/HomePageContent/Products.js
@@ -5,7 +5,7 @@ import ProductCard from "../../components/productCard";
 function Products() {
   return (
     <>
-      <div className="bg-white p-2 shadow-sm rounded-md">
+      <div className="bg-white p-2 shadow-sm rounded-md gap-3">
         <div className="text-lg text-[#247158] mb-3">
           <h1>Categories</h1>
           <hr></hr>
@@ -14,11 +14,26 @@ function Products() {
           <CategoryCard />
         </div>
       </div>
-      <div className="bg-white p-2 shadow-sm rounded-md">
-        <div className=" text-lg text-[#247158] mb-3">
+      <div className="bg-white p-2 shadow-sm rounded-md overflow-x-hidden scrollbar-hide overflow-y-auto ">
+        <div className=" text-lg text-[#247158] mb-3 px-5">
           <h1>Category chosen name</h1>
         </div>
-        <div className="flex flex-wrap justify-start gap-3">
+        <div className="flex flex-wrap gap-3  justify-center">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
           <ProductCard />
         </div>
       </div>
