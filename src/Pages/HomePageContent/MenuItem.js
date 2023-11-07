4d0eb64bedd952b65ace1950401d3f22de61fc1d
Emmanuel Abobila <masterxdee@gmail.com>
2023-11-07 07:36:52 +0000

    additional changes

--- src/Pages/HomePageContent/MenuItem.js
+++ src/Pages/HomePageContent/MenuItem.js
@@ -1,33 +1,40 @@
-import { AiOutlineHome } from 'react-icons/ai'
-import { BiLogoBootstrap } from 'react-icons/bi'
-import { BsCartCheck } from 'react-icons/bs'
-import { IoLogOutOutline } from 'react-icons/io5'
+import { AiOutlineHome } from "react-icons/ai";
+import { BiLogoBootstrap } from "react-icons/bi";
+import { BsCartCheck } from "react-icons/bs";
+import { IoLogOutOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 function MenuItem() {
   return (
     <div className="flex flex-col justify-between h-full ">
       <div>
-        <div>
-          <BiLogoBootstrap className="text-white text-6xl" />
-        </div>
-
+        <Link to={"/"}>
+          <div>
+            <BiLogoBootstrap className="text-white text-6xl" />
+          </div>
+        </Link>
         <div className="mt-3 flex flex-col justify-center ">
-          <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
-            <AiOutlineHome className="text-white text-3xl" />
-          </button>
-          <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
-            <BsCartCheck className="text-white text-3xl" />
-          </button>
+          <Link to={"/"}>
+            <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
+              <AiOutlineHome className="text-white text-3xl" />
+            </button>
+          </Link>
+          <Link to={"/register"}>
+            <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
+              <BsCartCheck className="text-white text-3xl" />
+            </button>
+          </Link>
         </div>
       </div>
-
       <div>
-        <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
-          <IoLogOutOutline className="text-white text-3xl" />
-        </button>
+        <Link to={"/login"}>
+          <button className=" flex justify-center hover:bg-[rgb(106,178,155)] mb-2  focus:bg-[#6AB29B] py-3 px-2 rounded  ">
+            <IoLogOutOutline className="text-white text-3xl" />
+          </button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default MenuItem
+export default MenuItem;
