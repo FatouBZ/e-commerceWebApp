4046d81a2b2fafd0391fc8e7aec4209745961948
FatouBZ <fbzongo@st.ug.edu.gh>
2023-11-13 15:00:47 +0000

    added profile and updated register ans login screen

--- src/Pages/HomePageContent/Products.js
+++ src/Pages/HomePageContent/Products.js
@@ -16,32 +16,19 @@ function Products() {
           <CategoryCard />
         </div>
       </div>
-      <div className="bg-white py-3 shadow-sm rounded-md overflow-x-hidden scrollbar-hide overflow-y-auto  overflow-x-hidden scrollbar-hide overflow-y-auto ">
-        <div className=" text-lg text-[#247158] mb-3 px-5 px-5">
+      <div className="bg-white py-3 shadow-sm rounded-md overflow-x-hidden scrollbar-hide overflow-y-auto ">
+        <div className=" text-lg text-[#247158] mb-3 px-5">
           <h1>Category chosen name</h1>
         </div>
         <div className="flex flex-wrap gap-3 px-5 justify-between">
           <OrdersTable />
           <ProductCard />
         </div>
-        <div className="flex flex-wrap gap-3  justify-center">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
+        <Link to="/product-details">
+          <div className="flex flex-wrap justify-start gap-3">
+            <ProductCard />
+          </div>
+        </Link>
       </div>
     </>
   )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -41,21 +41,21 @@ function Profile() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="flex x justify-start ">
+      <div className="flex w-[70%] justify-start ">
         <div className="flex flex-row w-full rounded-lg p-1">
           <div className="flex flex-col w-[100%] justify-center">
-            <h1 className="text-4xl text-[#247158] font-bold mb-5">
+            <h1 className="text-4xl text-[#fff] font-bold mb-5">
               Personal Information
             </h1>
             <div className="flex w-[100%]  mb-2">
               <img
-                className="w-32 h-32 outline outline-1 outline-[#247158] rounded-full bg-[#000]"
+                className="w-32 h-32 border-2 rounded-full bg-[#000]"
                 src="./asset/2.jpg"
                 alt="profile"
               />
-              <div className="text-[#247158]">
+              <div className="text-white">
                 <p className="flex ml-2 mt-10 mr-10 ">Fatou Bintou</p>
-                <p className=" ml-2 font-bold cursor-pointer underline ">
+                <p className=" ml-2 font-bold cursor-pointer hover:underline ">
                   Change
                 </p>
               </div>
@@ -64,7 +64,7 @@ function Profile() {
               <input
                 type="text"
                 placeholder="enter first name"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="firstname"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -78,7 +78,7 @@ function Profile() {
               <input
                 type="text"
                 placeholder="enter last name"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="lastname"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -92,7 +92,7 @@ function Profile() {
               <input
                 type="email"
                 placeholder="enter email"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="email"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -106,7 +106,7 @@ function Profile() {
               <input
                 type="password"
                 placeholder="enter password"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="password"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -120,7 +120,7 @@ function Profile() {
               <input
                 type="password"
                 placeholder="confirm your password"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="cpassword"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -134,7 +134,7 @@ function Profile() {
               <input
                 type="text"
                 placeholder="enter phone number"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-[#247158] bg-transparent  "
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
                 name="phone"
                 onBlur={handleBlur}
                 onChange={handleChange}
@@ -147,7 +147,7 @@ function Profile() {
             <div>
               <button
                 type="submit"
-                className="text-[#fff] rounded-md mt-3 bg-[#247158] font-bold w-full p-2"
+                className="text-[#247158] rounded-md mt-3 bg-[#fff] font-bold w-full p-2"
               >
                 Save
               </button>
