8fed07b3dcc3012604fc25d7960c8b8fe9231468
FatouBZ <fbzongo@st.ug.edu.gh>
2023-11-13 15:00:47 +0000

    added profile and updated register ans login screen

--- src/AuthenticationPage/Register.js
+++ src/AuthenticationPage/Register.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { BiShow, BiHide } from 'react-icons/bi'
 
 const initialValues = {
   firstname: '',
@@ -14,6 +14,16 @@ const initialValues = {
 }
 
 function Register() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showCPassword, setShowCPassword] = useState(false)
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const toggleCPasswordVisibility = () => {
+    setShowCPassword(!showCPassword)
+  }
   const registerSchema = Yup.object({
     email: Yup.string()
       .email('Invalid email address')
@@ -21,7 +31,9 @@ function Register() {
     firstname: Yup.string().required('Please enter first name'),
     lastname: Yup.string().required('Last name cannot be left blank'),
     password: Yup.string().required('Please enter password'),
-    cpassword: Yup.string().required('passwords do not match'),
+    cpassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Confirm Password is required'),
     phone: Yup.string().required('Please enter phone number'),
   })
 
@@ -35,24 +47,8 @@ function Register() {
   } = useFormik({
     initialValues: initialValues,
     validationSchema: registerSchema,
-    onSubmit: async (values) => {
-      try {
-        const response = await axios.post(
-          'http://localhost/eCommerce/register',
-          values,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-            },
-            withCredentials: true,
-          },
-        )
-
-        console.log('API request successful:', response.data)
-      } catch (error) {
-        console.error('API request failed:', error.message)
-      }
+    onSubmit: (values) => {
+      console.log(values)
     },
   })
 
@@ -114,34 +110,6 @@ function Register() {
                 <small className="text-red-500">{errors.email}</small>
               ) : null}
             </div>
-            <div className=" flex flex-col text-left">
-              <input
-                type="text"
-                placeholder="enter password"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                name="password"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.password}
-              />
-              {touched.password && errors.password ? (
-                <small className="text-red-500">{errors.password}</small>
-              ) : null}
-            </div>
-            <div className=" flex flex-col text-left">
-              <input
-                type="password"
-                placeholder="confirm your password"
-                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
-                name="cpassword"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.cpassword}
-              />
-              {touched.cpassword && errors.cpassword ? (
-                <small className="text-red-500">{errors.cpassword}</small>
-              ) : null}
-            </div>
             <div className=" flex flex-col text-left ">
               <input
                 type="text"
@@ -156,6 +124,60 @@ function Register() {
                 <small className="text-red-500">{errors.phone}</small>
               ) : null}
             </div>
+            <div className=" flex flex-col text-left relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="enter password"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+              />
+              {showPassword ? (
+                <BiShow
+                  onClick={togglePasswordVisibility}
+                  className="absolute top-2 right-2 text-xl"
+                />
+              ) : (
+                <BiHide
+                  onClick={togglePasswordVisibility}
+                  className="absolute top-2 right-2 text-xl"
+                />
+              )}
+
+              {touched.password && errors.password ? (
+                <small className="text-red-500">{errors.password}</small>
+              ) : null}
+            </div>
+            <div className=" flex flex-col text-left relative">
+              <input
+                type={showCPassword ? 'text' : 'password'}
+                placeholder="confirm your password"
+                className="block p-2 mb-2 rounded w-full outline outline-1 outline-gray-200"
+                name="cpassword"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.cpassword}
+              />
+
+              {showCPassword ? (
+                <BiShow
+                  onClick={toggleCPasswordVisibility}
+                  className="absolute top-2 right-2 text-xl"
+                />
+              ) : (
+                <BiHide
+                  onClick={toggleCPasswordVisibility}
+                  className="absolute top-2 right-2 text-xl"
+                />
+              )}
+
+              {touched.cpassword && errors.cpassword ? (
+                <small className="text-red-500">{errors.cpassword}</small>
+              ) : null}
+            </div>
+
             <div>
               <button
                 type="submit"
@@ -163,7 +185,7 @@ function Register() {
               >
                 Register
               </button>
-              <p className="text-gray-500  mt-2 ">
+              <p className="text-gray-500 ">
                 Already have an account?{' '}
                 <Link to={'/login'} className="text-[#2e8b36]">
                   Login here
