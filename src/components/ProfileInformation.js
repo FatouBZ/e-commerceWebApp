47da9c6a2964bd0009762d9732191e2f3211d23c
FatouBZ <fbzongo@st.ug.edu.gh>
2023-11-16 10:44:07 +0000

    update

--- src/components/ProfileInformation.js
+++ src/components/ProfileInformation.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ProfileMenuItems from '../Pages/HomePageContent/ProfileMenuItems'
-import Orders from './Orders'
-import Coupons from './Coupons'
 import AddressBook from './addressBook'
 
 function ProfileInformation() {
